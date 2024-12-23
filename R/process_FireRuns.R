48bd812249b67de9f70b337ed7f9315412666470
CYglume <glume.cy.yang@gmail.com>
2024-12-23 15:26:10 +0100

    update fetech_FireRun: finish all types of runs

--- R/process_FireRuns.R
+++ R/process_FireRuns.R
@@ -11,11 +11,11 @@ source("R/EnvSetup.R", echo = TRUE)
 # area_process("LC1", nameID = "ObjectID")
 # area_process("StLlorenc", nameFeho_i = "FeHo_2")
 
-areaList = list(c(aoi = "GIF14_Au",   nameID = "OBJECTID", nameFeho_i = "FeHo"),
-                c(aoi = "LaJonquera", nameID = "ObjectID", nameFeho_i = "FeHo"),
-                c(aoi = "LC1",        nameID = "ObjectID", nameFeho_i = "FeHo"),
-                c(aoi = "StLlorenc",  nameID = "OBJECTID", nameFeho_i = "FeHo_2")
-                )
+areaList = list(c(aoi = "GIF14_Au",   nameID = "OBJECTID",   nameFeho_i = "FeHo"),
+                c(aoi = "LaJonquera", nameID = "ObjectIDgo", nameFeho_i = "FeHo"),
+                c(aoi = "LC1",        nameID = "ObjectID",   nameFeho_i = "FeHo"),
+                c(aoi = "StLlorenc",  nameID = "OBJECTID",   nameFeho_i = "FeHo_2")
+)
 for (aa in areaList){
   area_process_allArrow(aa[['aoi']],  aa[['nameID']], aa[['nameFeho_i']])
 }
