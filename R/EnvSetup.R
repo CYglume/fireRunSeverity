eb62ac201cb8c07d27730008bd326efcb24883e8
CYglume <glume.cy.yang@gmail.com>
2024-12-24 00:48:04 +0100

    update folder structures

--- R/EnvSetup.R
+++ R/EnvSetup.R
@@ -1,7 +1,7 @@
 # Environment Set Up ------------------------------------------------------
 rm(list = ls())
 root_folder <- rprojroot::find_rstudio_root_file()
-dataDir <- r"(data\FirstWildfires)"
+dataDir <- r"(data/fireruns)"
 
 
 # Load Packages -----------------------------------------------------------
@@ -18,15 +18,38 @@ library(ggplot2)
 library(tidyterra)
 library(ggpubr)
 library(tidyverse)
-source("R/fun/Runs_AllArrows.R", echo = TRUE)
-source("R/fun/Process_Runs.R", echo = TRUE)
+source("R/fun/Runs_AllArrows.R", echo = FALSE)
+source("R/fun/Data_Function.R", echo = FALSE)
 
 
 
 #---- Create Data folder ------
-
-
-
-
+if (FALSE) {
+  dt_folder <- c("data/fireruns",
+                 "data/ER5",
+                 "data/GEE")
+  # Create project directories
+  for (dir in dt_folder) {
+    dir_path <- file.path(root_folder, dir)
+    if (!dir.exists(dir_path)) {
+      dir.create(dir_path, recursive = TRUE)
+      message(paste("Created folder:", dir))
+    } else {
+      message(cli::col_blue(paste("Folder exists:", dir)))
+    }
+  }
+  
+  # Create sub folders for fire runs
+  dt_folder <- list.dirs("src/fireRaw", recursive = F)
+  for (dir in dt_folder) {
+    dir_path <- file.path(root_folder, dir)
+    if (!dir.exists(dir_path)) {
+      dir.create(dir_path, recursive = TRUE)
+      message(paste("Created folder:", dir))
+    } else {
+      message(cli::col_blue(paste("Folder exists:", dir)))
+    }
+  }
+}
 #------------------------------
 
